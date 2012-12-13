b1644e797fa01c218d27dd73138a0790f337c9f9
Nicholai de Guzman <nicholai.deguzman@utsouthwestern.edu>
2012-12-13 13:25:23 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveySample::Application.config.secret_token = 'c1c2379a6dc4c8b443e3ef2dc8ebb2de09f06593b45ac448af1be68ed56b08dac9794945f7d9404fb375a654b711f39208725e0983adae6be19f723fd7269fba'
